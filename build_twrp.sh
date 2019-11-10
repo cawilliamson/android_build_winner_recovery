65672a3d6b80b4c01432fdbeb90fc2799e5dcb4b
Christopher A. Williamson <home@chrisaw.com>
2019-11-10 20:47:44 +0000

    Fix syntax errors in build script (twrp)

--- build_twrp.sh
+++ build_twrp.sh
@@ -6,9 +6,12 @@ docker run \
   -v $(pwd)/out:/out \
   buildrecovery \
   /bin/bash -c \
-  "mkdir -p /usr/src/recovery
-  cd /usr/src/recovery
-  repo init --depth=1 -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git -b twrp-9.0 && \
+  "mkdir -p /usr/src/recovery && \
+  cd /usr/src/recovery && \
+  repo init \
+    --depth=1 \
+    -u git://github.com/minimal-manifest-twrp/platform_manifest_twrp_omni.git \
+    -b twrp-9.0 && \
   mkdir -p .repo/local_manifests/ && \
   cp -v /common/manifests/local_manifest_winnerx.xml .repo/local_manifests/ && \
   repo sync -c -j$(nproc --all) --no-clone-bundle --no-tags && \
